2ae6d4d3b72d0054026bb69b1a28db315557e4ba
Liam Egan <liam@liamegan.com>
2021-02-20 12:30:03 -0500

    Remove template inheritance from the Template class

--- lib/documently/resources/template.rb
+++ lib/documently/resources/template.rb
@@ -1,30 +1,18 @@
 module Documently
   module Resources
     class Template
-      attr_reader :parent
-
-      def initialize(source:, engine:, parent: nil)
+      def initialize(source:, engine:)
         @source = source
         @engine = engine
-        @parent = parent
       end
 
       def render(assigns = {})
         flow = Template::Flow.new
         scope = Template::Scope.new(assigns)
 
-        template = self
-
-        until template.nil?
-          template.render_self(flow, scope)
-          template = template.parent
-        end
+        @engine.call(source: @source, flow: flow, scope: scope)
 
         flow.main
-      end
-
-      def render_self(flow, scope)
-        @engine.call(source: @source, flow: flow, scope: scope)
       end
     end
   end
--- test/unit/resources/template_test.rb
+++ test/unit/resources/template_test.rb
@@ -20,34 +20,6 @@ module Documently
 
         assert_equal "value", template.render({assign: "value"})
       end
-
-      test "passes the main section up the render chain" do
-        parent = Template.new(
-          source: "parent <%= content %>",
-          engine: Template::Engines::ERB.new
-        )
-        child = Template.new(
-          source: "child",
-          engine: Template::Engines::Text.new,
-          parent: parent
-        )
-
-        assert_equal "parent child", child.render
-      end
-
-      test "passes named sections up the render chain" do
-        parent = Template.new(
-          source: "parent <%= content_for :section %>",
-          engine: Template::Engines::ERB.new
-        )
-        child = Template.new(
-          source: "<% section :section do %>child<% end %>",
-          engine: Template::Engines::ERB.new,
-          parent: parent
-        )
-
-        assert_equal "parent child", child.render
-      end
     end
   end
 end
