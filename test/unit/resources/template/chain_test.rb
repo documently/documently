1f2d28b10e9ce2b45e39a4f10060a07c87f7f912
Liam Egan <liam@liamegan.com>
2021-02-24 15:38:15 -0500

    Reintroduce template inheritance through a "template chain"
    Most templating languages have the concepts of templates which "extend"
    other templates, typically indicated through a tag at the top of the
    file called "extends" or something of the like. However, in the context
    of a static site generator, I've been thinking less and less of pages as
    "inheriting from" other templates, and more of them "using" other
    templates as their base.
    
    The best word I could come up with to model this relationship was a
    "chain", as templates are rendered in order, passing the context along
    to each successive one. I think this word might cause confusion with
    the pipeline engine, which serves a similar, but unrelated purpose.
    Maybe better and more distinct words can be chosen for both, or maybe
    they're fine as-is.
    
    Regardless, I'm happy to separate this out from the core Template class.
    Having it in there created a weird disconnect between a template just
    rendering itself, and a template rendering itself and all its parents.
    Separating it out means that a Template is always responsible for
    rendering itself, and a Template::Chain is always responsible for
    rendering a sequence of Templates.

---
+++ test/unit/resources/template/chain_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+module Documently
+  module Resources
+    class Template
+      class ChainTest < TestCase
+        test "passes the main section up to each successive template" do
+          child = Template.new(
+            source: "child",
+            engine: Template::Engines::Text.new
+          )
+          parent = Template.new(
+            source: "parent <%= yield %>",
+            engine: Template::Engines::ERB.new
+          )
+          chain = Template::Chain.new([child, parent])
+
+          assert_equal "parent child", chain.render
+        end
+
+        test "passes named sections up to each successive template" do
+          child = Template.new(
+            source: "<% section :section do %>child<% end %>",
+            engine: Template::Engines::ERB.new
+          )
+          parent = Template.new(
+            source: "parent <%= yield :section %>",
+            engine: Template::Engines::ERB.new
+          )
+          chain = Template::Chain.new([child, parent])
+
+          assert_equal "parent child", chain.render
+        end
+
+        test "passes assigns to each template" do
+          child = Template.new(
+            source: "<%= assign %>",
+            engine: Template::Engines::ERB.new
+          )
+          parent = Template.new(
+            source: "<%= assign %> <%= yield %>",
+            engine: Template::Engines::ERB.new
+          )
+          chain = Template::Chain.new([child, parent])
+
+          assert_equal "value value", chain.render({assign: "value"})
+        end
+      end
+    end
+  end
+end
